7f7a726e9fd5b68198749e9166618af7d112102a
diogo <diogo.libras43@gmail.com>
2021-02-06 22:14:02 -0300

    commiting all project

---
+++ Repositories/IFundoCapitalRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Uniciv.Api.Models;
+
+namespace Uniciv.Api.Repositories
+{
+    public interface IFundoCapitalRepository
+    {
+        void Adicionar(FundoCapital fundo);
+        void Alterar(FundoCapital fundo);
+        IEnumerable<FundoCapital> ListaFundos();
+        FundoCapital ObterPorId(Guid id);
+        void RemoverFundo(FundoCapital fundo);
+
+    }
+}
